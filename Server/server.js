242d9fa79a2a7ead4364e1d3126cb98310c88aa1
GustaveRw <jmgustave5@gmail.com>
2022-03-16 13:38:50 +0200

    Added some other files

---
+++ Server/server.js
@@ -0,0 +1,59 @@
+var cluster = require('cluster');
+
+if (cluster.isMaster) {
+
+  
+  var cpuCount = require('os').cpus().length;
+
+  for (var i = 0; i < cpuCount; i += 1) {
+      cluster.fork();
+  }
+  cluster.on('exit', function (worker) {
+
+    console.log('Worker died :(', worker.id);
+    cluster.fork();
+
+    });
+
+} else {
+
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var api = require('./Routes/api');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ',app.get('port'));
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'../Public')));
+
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Method', 'GET, POST, HEAD, OPTIONS, PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization');
+  next();
+});
+
+app.use('/api', api);
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'../Public','tic-tac-toe.html'));
+});
+
+app.all('*', function (req, res) {
+  res.status(404).send('Nothing Here');
+});
+
+module.exports = server;
+}
